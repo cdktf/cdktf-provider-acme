ba42693b9c332346044c1d86a56fc893f649c0a5
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-02-12 12:25:07 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@b24ef08

--- src/registration/index.ts
+++ src/registration/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/vancluever/acme/2.20.0/docs/resources/registration
 // generated from terraform resource schema
 
